d68a8c2023b49ba2d0deeb3ccfc3232358a41e49
Vladislav V. Kurdyukov <funray.pro@gmail.com>
2015-01-07 00:41:01 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
  	validates :name, presence: true, length: { maximum: 50 }
@@ -10,9 +11,14 @@ class User < ActiveRecord::Base
   	validates :password, length: { minimum: 6 }
 	def User.new_remember_token
     	SecureRandom.urlsafe_base64
-  	end
+  end
 
-  	def User.encrypt(token)
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
+
+  def User.encrypt(token)
   	  Digest::SHA1.hexdigest(token.to_s)
  	end
 
