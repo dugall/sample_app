10e0680b66828e1a3b21dfeaa2ddc8a6d58b873d
Vladislav V. Kurdyukov <funray.pro@gmail.com>
2015-01-07 19:14:18 +0200

    added precompiled assets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,18 @@
-require 'rubygems'
-require 'spork'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  RSpec.configure do |config|
+RSpec.configure do |config|
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -37,22 +39,4 @@ Spork.prefork do
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  config.include Capybara::DSL
-  end
 end
-
-Spork.each_run do
-
-end
-
-
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-
-
-
